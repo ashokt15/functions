70b505a98f1770a095146c4d9607b658069fa78c
ashok <urashok15@gmail.com>
2022-07-07 11:26:49 +0530

    operators

---
+++ calculator.js
@@ -0,0 +1,4 @@
+function calculator(operation,x,y){
+    quotient=x/y;
+    console.log("the division result is" , quotient);
+}
---
+++ operators.js
@@ -0,0 +1,7 @@
+const now = 2022;
+const ageAshok  = now - 1992;
+const agePushpa = now - 1996;
+console.log(ageAshok, agePushpa);
+console.log(ageAshok *2, agePushpa /2);
+console.log(ageAshok > agePushpa);
+console.log(ageAshok >= 35)

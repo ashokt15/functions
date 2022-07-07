8f2855ed8f88c29748f164af72063ce42e6aea62
ashok <urashok15@gmail.com>
2022-07-07 13:36:45 +0530

    calculate

--- calculator.js
+++ calculator.js
@@ -1,4 +1,6 @@
-function calculator(operation,x,y){
-    quotient=x/y;
-    console.log("the division result is" , quotient);
+function calculate(operator,x,y){
+    if (operator == 'add'){return x+y};
+    if (operator == 'subtract'){ return x-y};
+    if (operator == 'multiply'){return x*y};
+    if (operator == 'divide'){ return x/y}; 
 }

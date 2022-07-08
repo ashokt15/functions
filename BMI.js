8a6a51710e75531a44ffba8e9969456ac77592c8
ashok <urashok15@gmail.com>
2022-07-08 15:40:52 +0530

    to find a BMI

---
+++ BMI.js
@@ -0,0 +1,10 @@
+function BMI(){
+    const massAshok = 62;
+    const heightAshok =1.69;
+    const BMIAshok = massAshok / heightAshok ((2;))
+const masssPushpa = 53;
+const heightPushpa = 1.5;
+
+const BMIAshok = massAshok / heightAshok **2;
+const BMIPushpa = massPushpa /(heightPushpa * heightPushpa) ;}
+const markHigherBMI = BMIAshok .BmiPushpa;
